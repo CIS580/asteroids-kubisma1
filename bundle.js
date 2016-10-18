f9ffcf93754fb7bf5a5b6ad583aba9ac241da548
Martin Kubis <mkubis@ksu.edu>
2016-10-17 22:16:28 -0500

    Bouncing asteroids
    TODO: Solve collisions with different masses

--- bundle.js
+++ bundle.js
@@ -264,6 +264,9 @@ SmallAsteroid.prototype.render = function(time, ctx) {
 const TOLERANCE = 10;
 const ASTEROID_COLOR = '#C90018';
 
+/* Classes */
+const Vector = require('./vector.js');
+
 /**
  * @module exports the EntityManager class
  */
@@ -324,6 +327,10 @@ function removeInvalidShots() {
   });
 }
 
+/**
+ * @function handleAsteroidsCollisions
+ * Goes over all asteroids and solves all their collisions
+ */
 function handleAsteroidsCollisions() {
 
   var active = [];
@@ -353,8 +360,48 @@ function handleAsteroidsCollisions() {
       collisions.push(pair);
     }
   });
+
+  collisions.forEach(function(pair) {
+    // find the normal of collision
+    var collisionNormal = {
+      x: pair.a.position.x - pair.b.position.x,
+      y: pair.a.position.y - pair.b.position.y
+    };
+
+    var overlap = pair.a.radius + pair.b.radius + 2 - Vector.magnitude(collisionNormal);
+    collisionNormal = Vector.normalize(collisionNormal);
+    pair.a.position.x += collisionNormal.x * overlap / 2;
+    pair.a.position.y += collisionNormal.y * overlap / 2;
+    pair.b.position.x -= collisionNormal.x * overlap / 2;
+    pair.b.position.y -= collisionNormal.y * overlap / 2;
+
+    // tg theta = y / x
+    // theta = acrtan (y/x)
+    var angle = Math.atan2(collisionNormal.y, collisionNormal.x);
+    // rotate the problem space so that the normalize
+    // of collision lies along the x-axis
+    var a = Vector.rotate(pair.a.velocity, angle);
+    var b = Vector.rotate(pair.b.velocity, angle);
+    // solve the collision along the x-axis
+    a.x = a.x * b.x;
+    b.x = a.x / b.x;
+    a.x = a.x / b.x;
+
+    // Rotate the problem space back to world space
+    a = Vector.rotate(a, -angle);
+    b = Vector.rotate(b, -angle);
+    pair.a.velocity.x = a.x;
+    pair.a.velocity.y = a.y;
+    pair.b.velocity.x = b.x;
+    pair.b.velocity.y = b.y;
+  });
 }
 
+/**
+ * @function handleAsteroidsCollisions
+ * Handles all collisions, between asteroids, asteroids and shots,
+ * asteroids and ship
+ */
 function handleCollisions() {
   this.asteroids.sort(function(a,b) {
     return a.position.x - b.position.x;
@@ -388,13 +435,13 @@ EntityManager.prototype.update = function(elapsedTime) {
 }
 
 /**
-  * @function render
-  * Calls a render method on all entities,
-  * all entites are being rendered into a back buffer.
-  * @param {DOMHighResTimeStamp} elapsedTime indicates
-  * the number of milliseconds passed since the last frame.
-  * @param {CanvasRenderingContext2D} ctx the context to render to
-  */
+ * @function render
+ * Calls a render method on all entities,
+ * all entites are being rendered into a back buffer.
+ * @param {DOMHighResTimeStamp} elapsedTime indicates
+ * the number of milliseconds passed since the last frame.
+ * @param {CanvasRenderingContext2D} ctx the context to render to
+ */
 EntityManager.prototype.render = function(elapsedTime, ctx) {
   this.asteroids.forEach(function(asteroid) {
     asteroid.render(elapsedTime, ctx);
@@ -405,7 +452,7 @@ EntityManager.prototype.render = function(elapsedTime, ctx) {
   this.player.render(elapsedTime, ctx);
 }
 
-},{}],4:[function(require,module,exports){
+},{"./vector.js":7}],4:[function(require,module,exports){
 "use strict";
 
 /**
@@ -688,6 +735,90 @@ Shot.prototype.render = function(time, ctx) {
 
   // Draw engine thrust
   ctx.restore();
+}
+
+},{}],7:[function(require,module,exports){
+"use strict";
+
+module.exports = exports = {
+  rotate: rotate,
+  dotProduct: dotProduct,
+  magnitude: magnitude,
+  normalize: normalize,
+  perpendicular: perpendicular,
+  findAxes: findAxes,
+  project: project
+}
+
+/**
+ * Stands for matrix multiplication  {x,y} * {{cos phi, -sin phi}, {sin phi, cos phi}}
+ */
+function add(a, b) {
+  return {
+    x: a.x + b.x,
+    y: a.y + b.y
+  }
+}
+
+function subtract(a, b) {
+  return {
+    x: a.x - b.x,
+    y: a.y - b.y
+  }
+}
+
+function rotate(a, angle) {
+  return {
+    x: a.x * Math.cos(angle) - a.y * Math.sin(angle),
+    y: a.x * Math.sin(angle) + a.y * Math.cos(angle)
+  }
+}
+
+function dotProduct(a, b) {
+  return a.x * b.x + a.y * b.y;
+}
+
+function magnitude(a) {
+  return Math.sqrt(a.x * a.x + a.y * a.y);
+}
+
+function normalize(a) {
+  var mag = magnitude(a);
+  return {
+    x: a.x / mag,
+    y: a.y / mag
+  };
+}
+
+function perpendicular(a) {
+  return {
+    x: -a.y,
+    y: a.x
+  }
+}
+
+function findAxes(shape) {
+  var axes = [];
+  shape.vertices.forEach(function(p1, i){
+    // find the adjacent vertex
+    var p2 = (shape.vertices.length == i+1) ? shape.vertices[0] : shape.vertices[i+1];
+    var edge = subtract(p2, p1);
+    var perp = perpendicular(edge);
+    var normal = normalize(perp);
+    axes.push(normal);
+  });
+  return axes;
+}
+
+function project(shape, axis){
+  var min = dotProduct(shape.vertices[0], axis);
+  var max = min;
+  for(var i = 1; i < shape.vertices.length; i++){
+    var p = dotProduct(shape.vertices[i], axis);
+    if(p < min) min = p;
+    else if(p > max) max = p;
+  }
+  return {min: min, max: max};
 }
 
 },{}]},{},[1]);
--- src/entity-manager.js
+++ src/entity-manager.js
@@ -3,6 +3,9 @@
 const TOLERANCE = 10;
 const ASTEROID_COLOR = '#C90018';
 
+/* Classes */
+const Vector = require('./vector.js');
+
 /**
  * @module exports the EntityManager class
  */
@@ -63,6 +66,10 @@ function removeInvalidShots() {
   });
 }
 
+/**
+ * @function handleAsteroidsCollisions
+ * Goes over all asteroids and solves all their collisions
+ */
 function handleAsteroidsCollisions() {
 
   var active = [];
@@ -92,8 +99,48 @@ function handleAsteroidsCollisions() {
       collisions.push(pair);
     }
   });
+
+  collisions.forEach(function(pair) {
+    // find the normal of collision
+    var collisionNormal = {
+      x: pair.a.position.x - pair.b.position.x,
+      y: pair.a.position.y - pair.b.position.y
+    };
+
+    var overlap = pair.a.radius + pair.b.radius + 2 - Vector.magnitude(collisionNormal);
+    collisionNormal = Vector.normalize(collisionNormal);
+    pair.a.position.x += collisionNormal.x * overlap / 2;
+    pair.a.position.y += collisionNormal.y * overlap / 2;
+    pair.b.position.x -= collisionNormal.x * overlap / 2;
+    pair.b.position.y -= collisionNormal.y * overlap / 2;
+
+    // tg theta = y / x
+    // theta = acrtan (y/x)
+    var angle = Math.atan2(collisionNormal.y, collisionNormal.x);
+    // rotate the problem space so that the normalize
+    // of collision lies along the x-axis
+    var a = Vector.rotate(pair.a.velocity, angle);
+    var b = Vector.rotate(pair.b.velocity, angle);
+    // solve the collision along the x-axis
+    a.x = a.x * b.x;
+    b.x = a.x / b.x;
+    a.x = a.x / b.x;
+
+    // Rotate the problem space back to world space
+    a = Vector.rotate(a, -angle);
+    b = Vector.rotate(b, -angle);
+    pair.a.velocity.x = a.x;
+    pair.a.velocity.y = a.y;
+    pair.b.velocity.x = b.x;
+    pair.b.velocity.y = b.y;
+  });
 }
 
+/**
+ * @function handleAsteroidsCollisions
+ * Handles all collisions, between asteroids, asteroids and shots,
+ * asteroids and ship
+ */
 function handleCollisions() {
   this.asteroids.sort(function(a,b) {
     return a.position.x - b.position.x;
@@ -127,13 +174,13 @@ EntityManager.prototype.update = function(elapsedTime) {
 }
 
 /**
-  * @function render
-  * Calls a render method on all entities,
-  * all entites are being rendered into a back buffer.
-  * @param {DOMHighResTimeStamp} elapsedTime indicates
-  * the number of milliseconds passed since the last frame.
-  * @param {CanvasRenderingContext2D} ctx the context to render to
-  */
+ * @function render
+ * Calls a render method on all entities,
+ * all entites are being rendered into a back buffer.
+ * @param {DOMHighResTimeStamp} elapsedTime indicates
+ * the number of milliseconds passed since the last frame.
+ * @param {CanvasRenderingContext2D} ctx the context to render to
+ */
 EntityManager.prototype.render = function(elapsedTime, ctx) {
   this.asteroids.forEach(function(asteroid) {
     asteroid.render(elapsedTime, ctx);
